4f8750fe61ac1bfedc8dff8a90834f50a6c48659
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 03:42:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |num, index|
-    index -= 1
+    index += 1
     puts "#{index}. #{num}"
   end
 end
